52f4c7c464a034919f621a617e8c4b56acb9d975
Mayur <mayurvijay.andhare@wsu.edu>
2019-09-04 12:41:05 -0700

    added OAuth 2.0 Demo example

---
+++ src/main/java/com/oauth/demo/springbootfacebooklogin/SpringBootFacebookLoginApplication.java
@@ -0,0 +1,13 @@
+package com.oauth.demo.springbootfacebooklogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFacebookLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFacebookLoginApplication.class, args);
+	}
+
+}
